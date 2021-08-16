6aa40a1d47c136d03da1afe9de215e2603583207
Alex Aguiñaga <marioalejandro300402@gmail.com>
2021-08-16 03:23:05 -0500

    Paginacion y filtrado local

--- src/App.js
+++ src/App.js
@@ -7,21 +7,22 @@ import { useForm } from 'react-hook-form';
 
 
 function App() {
-  const [apiResponse, setApiResponse] = useState([]);
-  const [filter, setFilter] = useState([]);
+  const [apiResponse, setApiResponse] = useState(null); 
+  const [allPokemons, setAllPokemons] = useState(null);
+  const [currentPage, setCurrentPage] = useState(0);
+  const [page, setPage] = useState(null);
+  const [failedSearch, setFailedSearch] = useState(false);
   const [pokemonsData, setPokemonsData] = useState([]);
 
   // Form
   const {register, handleSubmit} = useForm();
-  const [filterData, setFilterData] = useState({});
-
-  console.log(filterData)
+  const [filterData, setFilterData] = useState(null);
 
   // Get all pokemons
   useEffect(
     () => {
       const getPokemonsData = async () => {
-        const url = 'https://pokeapi.co/api/v2/pokemon?limit=1118&offset=0';
+        const url = 'https://pokeapi.co/api/v2/pokemon?limit=898&offset=0';
         const data = await serviceGet(url);
         setApiResponse(data);
       }
@@ -29,38 +30,111 @@ function App() {
     }, []
   );
 
-  // // Really filter
-  // useEffect(
-  //   () => {
-
-
-  //   }, [apiResponse]
-  // )
+  // All pokemons in a array
+  useEffect (
+    () => {
+      if (apiResponse) {
+        setAllPokemons (apiResponse.results)
+      }
+    }, [apiResponse]
+  );
 
   // Filter
   useEffect(
     () => {
-      if (apiResponse.results) {
-        for (let i = 0; i < 4; i++) {
-          setFilter(preview => [...preview, apiResponse.results[i]])
+      if (filterData) {
+        if (filterData.type !== '' && filterData.name !== '') {
+          const filteredPokemonsNameType = async () => {
+            // Type filter
+            const url = `https://pokeapi.co/api/v2/type/${filterData.type}`;
+            const data = await serviceGet(url);
+            const pokemonsByType = data.pokemon.map(element => element.pokemon);
+            // Name filter
+            const filterName = pokemonsByType.filter(element => element.name.indexOf(filterData.name.toLowerCase()) !== -1);
+            setAllPokemons(filterName)
+          }
+          filteredPokemonsNameType();
+        }
+        else {
+          // Type filter
+          if (filterData.type !== '') {
+            const filteredPokemonType = async () => {
+              const url = `https://pokeapi.co/api/v2/type/${filterData.type}`;
+              const data = await serviceGet(url);
+              const pokemonsByType = data.pokemon.map(element => element.pokemon);
+              setAllPokemons(pokemonsByType);
+            }
+            filteredPokemonType();
+          }
+          // Name Filter
+          if (filterData.name !== '') {
+            const filteredPokemonName = async () => {
+              const filterName = apiResponse.results.filter(element => element.name.indexOf(filterData.name.toLowerCase()) !== -1);
+              setAllPokemons(filterName)
+            }
+            filteredPokemonName();
+          }
+          // Not filtered
+          if ( filterData.name === '' && filterData.type === '' ) {
+            setAllPokemons(apiResponse.results);
+          }
         }
       }
-      console.log(apiResponse);
-    }, [apiResponse]
+    }, [filterData]
   )
+
+  // Failed Search
   useEffect(
     () => {
-      if (filter) {
-        filter.forEach(element => {
+      if (allPokemons) {
+        if (allPokemons.length === 0) {
+          setFailedSearch(true)
+        }
+        else {
+          setFailedSearch(false);
+        }
+      }
+
+    }, [allPokemons]
+  )
+  // Pagination
+  useEffect (
+    () => {
+      if (allPokemons) {
+        const numberOfPages =  Math.ceil(allPokemons.length / 4);
+        setPage(allPokemons.slice(currentPage, currentPage +4));
+      }
+    }, [allPokemons, currentPage]
+  )
+  const onChangePage = (num) => {
+    setCurrentPage(prev => prev +num*4);
+  }
+  const onChangePageGroup = (action) => {
+    if (action === 'next') {
+      setCurrentPage(prev => prev +40)
+    }
+    if (action === 'back') {
+      setCurrentPage(prev => prev -40)
+    }
+  }
+
+  // Fetch details
+  useEffect(
+    () => {
+      if (page) {
+        page.forEach(element => {
           const getPokemonData = async (element) => {
+            setPokemonsData([])
             element = await serviceGet(element.url);
             setPokemonsData(data => [...data, element])
           };
           getPokemonData(element)
         });
       }
-    }, [filter]
+    }, [page]
   )
+
+  // Order
   useEffect(
     () => {
       if (pokemonsData) {
@@ -68,9 +142,32 @@ function App() {
       }
     }, [pokemonsData]
   )
+
   return (
     <div>
-      <PokedexContainer pokemonsData={pokemonsData} register={register} handleSubmit={handleSubmit} setFilterData={setFilterData} />
+      <PokedexContainer
+      pokemonsData={pokemonsData}
+      register={register}
+      handleSubmit={handleSubmit}
+      setFilterData={setFilterData}
+      failedSearch={failedSearch} />
+      <div>
+        <button onClick={()=> onChangePageGroup('back')} >back</button>
+        <button onClick={()=> onChangePageGroup('next')} >next</button>
+        <button onClick={() => onChangePage(0)} >1</button>
+        <button onClick={() => onChangePage(1)} >2</button>
+        <button onClick={() => onChangePage(2)} >3</button>
+        <button onClick={() => onChangePage(3)} >4</button>
+        <button onClick={() => onChangePage(4)} >5</button>
+        <button onClick={() => onChangePage(5)} >6</button>
+        <button onClick={() => onChangePage(6)} >7</button>
+        <button onClick={() => onChangePage(7)} >8</button>
+        <button onClick={() => onChangePage(8)} >9</button>
+        <button onClick={() => onChangePage(9)} >10</button>
+      </div>
+
+
+
       {/* <PokemonContainer pokemon={pokemonsData[0]}/> */}
 
     </div>
--- src/componets/PokedexContainer.js
+++ src/componets/PokedexContainer.js
@@ -3,7 +3,7 @@ import { serviceGet } from "../services/get"
 
 import { useForm } from 'react-hook-form';
 
-const PokedexContainer = ({ pokemonsData, register, handleSubmit, setFilterData }) => {
+const PokedexContainer = ({ pokemonsData, register, handleSubmit, setFilterData, failedSearch }) => {
 
   const pokemonsList = pokemonsData.map(element => {
     // Types
@@ -17,8 +17,8 @@ const PokedexContainer = ({ pokemonsData, register, handleSubmit, setFilterData 
     return (
       <div className={`pokedexContainer__pokemon col-11 color-${element.types[0].type.name}`} key={element.id} >
         <div className='ms-3' >
-          <p className='m-0 mt-1' > {`#${element.id}`} </p>
-          <h2 className='color-white m-0' > {element.name} </h2>
+          <p className={`m-0 mt-1 specialColor-${element.types[0].type.name}`} > {`#${element.id}`} </p>
+          <h2 className='color-white f-mayus m-0' > {element.name} </h2>
           <div className='mt-2 mb-3 d-flex' >{typesList}</div>
         </div>
         <img className='pokedexContainer__pokemon__image'  
@@ -33,18 +33,36 @@ return (
       <header></header>
       <div className='row mt-3' >
         <h1 className='col-sm-2 mb-1' >Pokédex</h1>
-        <p className='col-sm-10 color-black-50 my-sm-auto' >Search for Pokémon by name or using the National Pokédex number.</p>
+        <p className='col-sm-10 color-black-50 my-sm-auto' >Search for Pokémon by name or using the type selector.</p>
       </div>
       <div className='row' >
-        <form className='pokedexContainer__searchBox col-11 col-sm-12 mt-2 p-1 m-auto' 
+        <form className={`pokedexContainer__searchBox col-11 col-sm-12 mt-2 p-2 m-auto failedSearch-${failedSearch}`} 
         onSubmit={handleSubmit((data) => setFilterData(data) )} >
           <button type='submit' ><i className="bi bi-search"></i></button>
-          <input placeholder='What Pokémon are you looking for?' {...register('pokemon')} />
-          <select {...register('type')}>
-            <option value='fire' > fire </option>
-            <option value='grass' > grass </option>
+          <input placeholder='What Pokémon are you looking for?' {...register('name')} />
+          <select {...register('type')} >
+            <option value='' > All types </option>
+            <option value='normal' > Normal </option>
+            <option value='fire' > Fire </option>
+            <option value='water' > Water </option>
+            <option value='electric' > Electric </option>
+            <option value='grass' > Grass </option>
+            <option value='ice' > Ice </option>
+            <option value='fighting' > Fighting </option>
+            <option value='poison' > Poison </option>
+            <option value='ground' > Ground </option>
+            <option value='flying' > Flying </option>
+            <option value='psychic' > Psychic </option>
+            <option value='bug' > Bug </option>
+            <option value='rock' > Rock </option>
+            <option value='ghost' > Ghost </option>
+            <option value='dragon' > Dragon </option>
+            <option value='dark' > Dark </option>
+            <option value='steel' > Steel </option>
+            <option value='fairy' > Fairy </option>
           </select>
         </form>
+        {failedSearch && <p className='text-center' style={{color:'red'}} >I can't find your pokemon :c</p>}
       </div>
       <div className='pokedexContainer__pokemonsList mt-4' >
         {pokemonsList}
